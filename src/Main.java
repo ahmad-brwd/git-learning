bc8cd3d70e5e1553bb882ab7f86316fc2d202ab6
ahmad-brwd <ahmadbrwdarsalan@gmail.com>
2023-09-05 12:36:56 +0530

    Added msg from Abdul

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     // Main method implementation
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world! from Abdul");
     }
 }
